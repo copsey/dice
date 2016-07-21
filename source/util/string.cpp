8f6c914833e44abc76d439121909b996dd85b0f4
Jack Copsey <jack.copsey@yahoo.com>
2016-07-21 11:38:20 +0100

    Add pad_front

--- source/util/string.cpp
+++ source/util/string.cpp
@@ -4,8 +4,8 @@
 
 #include "string.hpp"
 
-std::vector<std::string> dice::util::split_and_prune (const std::string & str) {
-   std::vector<std::string> substrings;
+std::vector<std::string> dice::util::split_and_prune(const std::string & str) {
+   std::vector<std::string> substrings{};
 
    auto i = str.begin();
    auto j = i;
@@ -25,7 +25,7 @@ std::vector<std::string> dice::util::split_and_prune (const std::string & str) {
    return substrings;
 }
 
-int dice::util::to_i (const std::string & str, int base) {
+int dice::util::to_i(const std::string & str, int base) {
    std::size_t pos;
    auto i = std::stoi(str, &pos, base);
 
@@ -34,4 +34,8 @@ int dice::util::to_i (const std::string & str, int base) {
    }
 
    return i;
+}
+
+void dice::util::pad_front(std::string & str, char ch, std::string::size_type min_l) {
+   str.insert(0, (str.size() < min_l) ? (min_l - str.size()) : 0, ch);
 }
--- source/util/string.hpp
+++ source/util/string.hpp
@@ -10,14 +10,21 @@ namespace dice {
       /// by whitespace in `str`.
       ///
       /// Empty substrings are automatically removed.
-      std::vector<std::string> split_and_prune (const std::string & str);
+      std::vector<std::string> split_and_prune(const std::string & str);
 
       /// Convert `str` into an `int`, using the given numeric base.
       ///
-      /// In addition to throwing the same exceptions as `std::stoi(str, 0, base)`,
-      /// a `std::invalid_argument` is thrown if some unexpected superfluous
-      /// `char`s were encountered at the end of `str`.
-      int to_i (const std::string & str, int base = 10);
+      /// In addition to throwing the same exceptions as
+      /// `std::stoi(str, 0, base)`, a `std::invalid_argument` is thrown if some
+      /// unexpected superfluous `char`s were encountered at the end of `str`.
+      int to_i(const std::string & str, int base = 10);
+
+      /// Insert copies of `ch` at the front of `str` until its length is at
+      /// least `min_l`.
+      ///
+      /// If `str.size()` is already `min_l` or more, this function has no
+      /// effect.
+      void pad_front(std::string & str, char ch, std::string::size_type min_l);
    }
 }
 
