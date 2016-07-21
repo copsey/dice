0a05537c1b18e6c3b72c8e9d9e2e5cb93fb31386
Jack Copsey <jack.copsey@yahoo.com>
2016-07-21 11:52:02 +0100

    Refactor cli.hpp/cpp and numeric.hpp

--- source/util/cli.cpp
+++ source/util/cli.cpp
@@ -1,11 +1,11 @@
 #include "cli.hpp"
 
-std::vector<std::string> dice::util::encapsulate_args (int arg_c, const char * arg_v[]) {
-   std::vector<std::string> args;
+std::vector<std::string> dice::util::encapsulate_args(int arg_c, const char * arg_v[]) {
+   std::vector<std::string> args{};
    for (int i = 0; i < arg_c; ++i) args.emplace_back(arg_v[i]);
    return args;
 }
 
-bool dice::util::is_clo (const std::string & str) {
+bool dice::util::is_clo(const std::string & str) {
    return !str.empty() && str.front() == '-';
 }
--- source/util/numeric.hpp
+++ source/util/numeric.hpp
@@ -7,7 +7,7 @@ namespace dice {
    namespace util {
       /// Calculate the sum of the elements in `c`, starting from 0.
       template <class Cont>
-      typename Cont::value_type sum (const Cont & c) {
+      typename Cont::value_type sum(const Cont & c) {
          using T = typename Cont::value_type;
          return std::accumulate(c.begin(), c.end(), T{0});
       }
